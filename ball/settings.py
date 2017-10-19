a79154d38356bf50b5334aeeb2378d1cccbacbf2
itimor <itimor@techdog.com>
2017-10-19 14:27:56 +0800

    change page size

--- ball/settings.py
+++ ball/settings.py
@@ -120,7 +120,7 @@ MEDIA_URL = '/upload/'
 REST_USE_JWT = True
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 100,
     'DEFAULT_PERMISSION_CLASSES': (
         #'rest_framework.permissions.IsAuthenticated',
         'rest_framework.permissions.AllowAny',

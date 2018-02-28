3fd0e1b38040925a65c562433c62d329fc90af6b
kiven <kiven@126.com>
2018-02-28 15:00:15 +0800

    add

--- spider/soccer_spider/soccer_spider/pipelines.py
+++ spider/soccer_spider/soccer_spider/pipelines.py
@@ -5,7 +5,7 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
-import settings
+from . import settings
 from PIL import Image
 import math
 

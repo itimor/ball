e3fd12a428dd03a8d2198111633b7aa65927e946
kevin <kevin@126.com>
2018-03-05 14:11:25 +0800

    fix model

--- spider/soccer_spider/soccer_spider/items.py
+++ spider/soccer_spider/soccer_spider/items.py
@@ -5,7 +5,6 @@
 # See documentation in:
 # http://doc.scrapy.org/en/latest/topics/items.html
 
-import scrapy
 from scrapy_djangoitem import DjangoItem
 from soccer.models import *
 
@@ -14,24 +13,24 @@ class News(DjangoItem):
     django_model = SoccerNews
 
 
-class Match(scrapy.Item):
+class Match(DjangoItem):
     django_model = SoccerMatch
 
 
 # 当前各大比赛的状态
-class NowCompInfo(scrapy.Item):
+class NowCompInfo(DjangoItem):
     django_model = NowCompInfo
 
 
 # 当前某个队伍积分
-class TeamJifen(scrapy.Item):
+class TeamJifen(DjangoItem):
     django_model = SoccerTeamJifen
 
 
 # 射手
-class Shooter(scrapy.Item):
+class Shooter(DjangoItem):
     django_model = SoccerShooter
 
 
-class MatchEuropeLottery(scrapy.Item):
+class MatchEuropeLottery(DjangoItem):
     django_model = SoccerMatchEurope

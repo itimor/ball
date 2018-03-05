25afcb465ec87a14300624b01cc8afd6f88266ee
kevin <kevin@126.com>
2018-03-05 12:07:45 +0800

    up proxy ip pool

--- spider/soccer_spider/soccer_spider/settings.py
+++ spider/soccer_spider/soccer_spider/settings.py
@@ -99,12 +99,12 @@ USER_AGENTS = [
 ]
 
 PROXIES = [
-    {'ip_port': '111.11.228.75:80', 'user_pass': ''},
-    {'ip_port': '120.198.243.22:80', 'user_pass': ''},
-    {'ip_port': '111.8.60.9:8123', 'user_pass': ''},
-    {'ip_port': '101.71.27.120:80', 'user_pass': ''},
-    {'ip_port': '122.96.59.104:80', 'user_pass': ''},
-    {'ip_port': '122.224.249.122:8088', 'user_pass': ''},
+    {'ip_port': '221.227.250.84:21886', 'user_pass': ''},
+    {'ip_port': '115.217.252.225:38249', 'user_pass': ''},
+    {'ip_port': '122.114.31.177:808', 'user_pass': ''},
+    {'ip_port': '61.135.217.7:80', 'user_pass': ''},
+    {'ip_port': '120.92.88.202:10000', 'user_pass': ''},
+    {'ip_port': '123.169.254.75:61234', 'user_pass': ''},
 ]
 
 COOKIES_ENABLED = False

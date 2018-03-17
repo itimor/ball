f4067e4a5fca6dcc8c88f82751febfbaecbc9c5a
kevin <kevin@126.com>
2018-03-17 18:36:25 +0800

    add socer+-

---
+++ utils/gailv.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# author: itimor
+# 计算赔率代表的胜负概率
+
+
+def gvcount(x, y, z):
+    total = 1 / x + 1 / y + 1 / z
+    x_ = 1 / x / total * 100
+    y_ = 1 / y / total * 100
+    z_ = 1 / z / total * 100
+    return [x_, y_, z_]
+
+
+if __name__ == '__main__':
+    x = 2.10
+    y = 3.40
+    z = 3.90
+    print(gvcount(x, y, z))
---
+++ utils/socer+-.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+# author: itimor
+# desc: 计算各欧赔公司的赔率胜负概率与实际赛果有出入时，计算加分还是扣分
+
+from gailv import gvcount
+
+
+def rank_socer(s, p, f, real):
+    """
+    s: 胜的赔率
+    p: 胜的赔率
+    z: 胜的赔率
+    real: 赛果 - s|p|f
+    """
+    # 计算胜平负概率
+    d = gvcount(s, p, f)
+    dc = dict()
+    dc['s'] = d[0]
+    dc['p'] = d[1]
+    dc['f'] = d[2]
+    d_max = max(dc['s'], dc['p'], dc['f'])
+    dd = dict()
+    if d_max == dc['s']:
+        dd['d_real'] = 's'
+        dd['d_socer'] = dc['s']
+    elif d_max == dc['p']:
+        dd['d_real'] = 'p'
+        dd['d_socer'] = dc['p']
+    else:
+        dd['d_real'] = 'f'
+        dd['d_socer'] = dc['f']
+
+    if dd['d_real'] == real:
+        return 1
+    else:
+        diff = dd['d_socer'] - dc[real]
+        if abs(diff) > 20.998:
+            return -2
+        else:
+            return -1
+
+
+if __name__ == '__main__':
+    x = 2.10
+    y = 3.40
+    z = 3.90
+    print(rank_socer(x, y, z, 'f'))
